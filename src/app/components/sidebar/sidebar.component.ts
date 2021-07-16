5670bebb661fa3ed91f186390aba73598a7a92b5
ramms44 <madeapps11@gmail.com>
2021-07-16 20:22:49 +0700

    commit pertama

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from "@angular/core";
+
+declare const $: any;
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: "/user-profile", title: "User Profile", icon: "person", class: "" },
+  { path: "/dashboard", title: "Dashboard", icon: "dashboard", class: "" },
+  {
+    path: "/table-list",
+    title: "Manage Leads",
+    icon: "content_paste",
+    class: "",
+  },
+  {
+    path: "/typography",
+    title: "Quotations",
+    icon: "library_books",
+    class: "",
+  },
+  { path: "/icons", title: "Sales", icon: "bubble_chart", class: "" },
+  // { path: "/maps", title: "Maps", icon: "location_on", class: "" },
+  // {
+  //   path: "/notifications",
+  //   title: "Sales",
+  //   icon: "notifications",
+  //   class: "",
+  // },
+  // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"],
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+  }
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  }
+}
